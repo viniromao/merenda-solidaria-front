254e7d2df1feefe5dfb46cfd8c0d46672ca2bf10
Ricardo <ricardosantossiqueira@poli.ufrj.br>
2020-05-23 17:11:45 -0300

    login funcionando

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import "./Login.css";
+import { handleLogin } from "../../services/auth";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Login() {
-  const history = useHistory();
+  const { data } = useSelector((state) => {
+    return state.auth;
+  });
 
-  const loginHandle = () => {
-    history.push("/receiver");
-  };
+  const [email, setEmail] = useState("char_lec@gmail.com");
+  const [password, setPassword] = useState("abelha123");
+
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    data.access_token && history.push("/receiver");
+  }, [data, history]);
 
   return (
     <div id="login-main-div">
       <div id="login-header"></div>
       <h3>Entre ou registre-se</h3>
-      <input placeholder="Email" className="login-input-div"></input>
-      <input placeholder="Senha" className="login-input-div"></input>
+      <input
+        placeholder="Email"
+        className="login-input-div"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      ></input>
+      <input
+        placeholder="Senha"
+        className="login-input-div"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      ></input>
       <div id="login-bottom-div">
-        <div onClick={() => loginHandle()} id="login-bottom-div-buttom">
+        <button
+          onClick={() =>
+            handleLogin({ email, password, keepConnected: false, dispatch })
+          }
+          id="login-bottom-div-buttom"
+        >
           FAZER LOGIN
-        </div>
+        </button>
         <span id="login-bottom-div-register-span">
           Não possui uma conta?<span>Cadastre-se</span>
         </span>
