9e425224dc805f13bf5af545c7a82a805e74f20d
viniciusromaodossantos <viniromasan@poli.ufrj.br>
2020-05-23 17:41:44 -0300

    3

---
+++ src/pages/Register/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import "./Register.css";
+
+export default function Register() {
+  const history = useHistory();
+  const loginHandle = () => {
+    history.push("/receiver");
+  };
+  return (
+    <div id="reg-main-div">
+      <h5>Nos dê algumas informações importantes</h5>
+      <input className="reg-input" placeholder="Email"></input>
+      <input className="reg-input" placeholder="Primeiro Nome"></input>
+      <input className="reg-input" placeholder="Sobrenome"></input>
+      <input className="reg-input" placeholder="Telefone"></input>
+      <input type="password" className="reg-input" placeholder="Senha"></input>
+      <input
+        type="password"
+        className="reg-input"
+        placeholder="Confirmação da senha"
+      ></input>
+      <input className="reg-input" placeholder="Endereço residencial"></input>
+      <button onClick={() => loginHandle()} id="reg-bottom-div-buttom">
+        CONTINUAR
+      </button>
+    </div>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Receiver from "./pages/Receiver";
 import Initial from "./pages/Initial";
 import Provider from "./pages/Provider";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
@@ -12,6 +13,7 @@ const Routes = () => (
     <Switch>
       <Route exact path="/" component={() => <Initial />} />
       <Route path="/login" component={() => <Login />} />
+      <Route path="/register" component={() => <Register />} />
       <Route path="/landing" component={() => <Landing />} />
       <Route path="/receiver" component={() => <Receiver />} />
       <Route path="/provider" component={() => <Provider />} />
