47b0bb12d79633aa4d956a1894ed1b789d7cb32c
viniciusromaodossantos <viniromasan@poli.ufrj.br>
2020-05-23 10:46:06 -0300

    Updates

---
+++ precache-manifest.5ce33c38c0db5d8d7330bd2581bd1ac4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7495f5d6ce4f30cbf3fc5b7122237923",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9842e1f2cce66c9fa3b0",
+    "url": "/static/css/main.f67f0613.chunk.css"
+  },
+  {
+    "revision": "0cce8513ff3246c7559a",
+    "url": "/static/js/2.bd236e1e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bd236e1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9842e1f2cce66c9fa3b0",
+    "url": "/static/js/main.52e84680.chunk.js"
+  },
+  {
+    "revision": "16bad6ef82f4be118afe",
+    "url": "/static/js/runtime-main.b9e95170.js"
+  },
+  {
+    "revision": "97b713db399468662f4054408c632a56",
+    "url": "/static/media/cesta.97b713db.svg"
+  },
+  {
+    "revision": "c6c3a4ce4522a820880dc569f408a719",
+    "url": "/static/media/comer.c6c3a4ce.svg"
+  }
+]);
